df5fd31a258173531ac91e19078a4b9a877d433b
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-10-23 19:29:38 +0200

    Add tests

---
+++ test/blog_fetch_test.dart
@@ -0,0 +1,56 @@
+import 'package:test/test.dart';
+import 'package:bocconi_radio/blog/blog.dart';
+import 'package:webfeed/domain/rss_item.dart';
+
+void main() {
+  group('Download posts', () {
+    test('Find recent posts', () async {
+      areThereSomePosts(
+        await Blog.getPosts()
+      );
+    });
+
+    test('Find older posts', () async {
+      areThereSomePosts(
+        await Blog.getPosts(start: 40)
+      );
+    });
+
+    test('Find no posts (end of pagination)', () async {
+      final posts = await Blog.getPosts(
+        start: 2000,
+      );
+
+      expect(posts?.length, equals(0));
+      expect(posts?.isEmpty, isTrue);
+    });
+
+    test('Acceptable download and parse time', () async {
+      final measures = await download(50);
+      final total = measures.reduce((a, b) => a + b);
+      final mean = total / measures.length;
+
+      expect(mean, lessThan(600));
+    });
+  });
+}
+
+
+areThereSomePosts(List<RssItem>? posts) {
+  expect(posts?.length, greaterThan(0));
+  expect(posts?.isNotEmpty, isTrue);
+  expect(posts?.first.author, isNotNull);
+}
+
+
+Future<List<int>> download(int howMany) {
+  return Future.wait(
+    List.filled(howMany, measureElapsed())
+  );
+}
+
+Future<int> measureElapsed() async {
+  var stopwatch = Stopwatch()..start();
+  await Blog.getPosts();
+  return stopwatch.elapsedMilliseconds;
+}
