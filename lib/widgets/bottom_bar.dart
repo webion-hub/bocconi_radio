1968486ca29e9c4efcdf3ae981d8e42745d8e0f9
matteo <matteo.budriesi@gmail.com>
2021-10-28 18:02:56 +0200

    create bottom bar

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,48 +1,54 @@
-import 'dart:developer';
 
-import 'package:bocconi_radio/pages/home_page.dart';
-import 'package:bocconi_radio/pages/webcam_page.dart';
+import 'package:bocconi_radio/widgets/icon_button_with_label.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class BottomBar extends StatefulWidget {
-  Function(int) indexCallback;
+  String currentRoute;
+  GlobalKey<NavigatorState> navigatorKey;
 
-  BottomBar(this.indexCallback, {Key? key}) : super(key: key);
+  BottomBar({
+    this.currentRoute = "",  
+    required this.navigatorKey,
+    Key? key
+  }) : super(key: key);
 
   @override
   _BottomBarState createState() => _BottomBarState();
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int index = 0;
-
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: index,
-      onTap: (value) {
-        setState(() => index = value);
-        widget.indexCallback(value);
-      },
-      items: const [
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home_rounded)
-        ),
-        BottomNavigationBarItem(
-          label: 'Webcam',
-          icon: Icon(Icons.videocam_rounded)),
-        BottomNavigationBarItem(
-          label: 'Blog',
-          icon: Icon(Icons.article_rounded)
-        ),
-        BottomNavigationBarItem(
-          label: 'Info',
-          icon: Icon(Icons.info_rounded)
-        )
-      ],
+
+    bool isActive(String url){
+      return widget.currentRoute == url;
+    }
+
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButtonWithLabel(
+            active: isActive("/blog"),
+            onPressed: (){
+              if(isActive('/blog')){return;}
+              widget.navigatorKey.currentState?.pushNamed("/blog");
+            },
+            text: "Blog",
+            icon: Icons.feed_rounded     
+          ),
+          IconButtonWithLabel(
+            active: isActive("/webcam"),
+            onPressed: (){
+              if(isActive('/webcam')){return;}
+              widget.navigatorKey.currentState?.pushNamed('/webcam');
+            },
+            text: "Webcam",
+            icon: Icons.videocam_rounded   
+          ),
+        ],
+      ),
     );
   }
 }
