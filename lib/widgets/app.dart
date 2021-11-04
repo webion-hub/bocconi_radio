ba40955c368eef0a78746216271ca25b43366f2e
matteo <matteo.budriesi@gmail.com>
2021-11-04 12:25:09 +0100

    update app.dart

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -2,71 +2,46 @@ import 'package:bocconi_radio/pages/blog_page.dart';
 import 'package:bocconi_radio/pages/webcam_page.dart';
 import 'package:bocconi_radio/widgets/bottom_bar.dart';
 import 'package:bocconi_radio/pages/home_page.dart';
+import 'package:bocconi_radio/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/subjects.dart';
 
 class App extends StatefulWidget {
-
-  const App({
-    Key? key
-  }) : super(key: key);
+  const App({ Key? key }) : super(key: key);
 
   @override
   State<App> createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  final _navigatorKey = GlobalKey<NavigatorState>();
-  String currentRoute = '/';
-
-  void updateCurrentRoute(String newRoute){
-    setState(() {
-      currentRoute = newRoute;
-    });
-  }
+  BehaviorSubject<int> stream = BehaviorSubject<int>();
 
   @override
   Widget build(BuildContext context) {
-    const String initialRoute = '/';
 
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.17,
-        width: MediaQuery.of(context).size.width * 0.17,
-        child: FloatingActionButton(
-          child: const Icon(Icons.mic_rounded, size: 28), 
-          onPressed: () {
-            _navigatorKey.currentState?.pushNamed("/");
-          },
-        )
-      ),
-      bottomNavigationBar: BottomBar(
-        navigatorKey: _navigatorKey,
-        currentRoute: currentRoute,
-      ),
-      appBar: AppBar(),
-      body: Navigator(
-        key: _navigatorKey,
-        initialRoute: '/',
-        onGenerateRoute: (RouteSettings settings) {
-          String route = settings.name ?? initialRoute;
+    StatelessWidget getBody(int pageIndex){
+      const widgets = {
+        0: HomePage(),
+        1: BlogPage(),
+        2: WebcamPage(),
+      };
 
-          return MaterialPageRoute(
-            settings: settings,
-            builder: (BuildContext context) {
-              const routes = {
-                '/webcam': WebcamPage(),
-                '/blog':   BlogPage(),
-                '/':       HomePage(),
-              };
+      return widgets[pageIndex] ?? const HomePage();
+    }
 
-              updateCurrentRoute(route);
-              return routes[route] ?? const HomePage();
-            },
-          );
-        }
-      )
+    return StreamBuilder(
+      stream: stream,
+      initialData: 0,
+      builder: (context, AsyncSnapshot<int> snapshot){
+        return Scaffold(
+          backgroundColor: Theme.of(context).backgroundColor,
+          appBar: const CustomAppbar(),
+          bottomNavigationBar: BottomBar(
+            onPressed: stream.add,
+          ),
+          body: getBody(snapshot.data ?? 0)
+        );
+      }
     );
   }
 }
- 
