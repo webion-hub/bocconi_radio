0d4eb21540800d41b8b2714fc2baf021c95eb69d
matteo <matteo.budriesi@gmail.com>
2021-11-04 12:27:06 +0100

    create custom app bar with info button

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,36 @@
+import 'package:bocconi_radio/pages/info_page.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget with PreferredSizeWidget{
+  const CustomAppbar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.info_rounded),
+          onPressed: (){
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16), 
+                  topRight: Radius.circular(16)
+                )
+              ),
+              context: context,
+              builder: (BuildContext context) {
+                return const InfoPage(); 
+              },
+            );
+          }, 
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
