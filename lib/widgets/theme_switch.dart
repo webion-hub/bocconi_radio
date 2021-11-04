267081c28640670b9e1648f76ba0f6ec59cf1775
matteo <matteo.budriesi@gmail.com>
2021-11-04 12:27:33 +0100

    start creation of theme switch

---
+++ lib/widgets/theme_switch.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ThemeSwitch extends StatefulWidget {
+
+  const ThemeSwitch({ Key? key }) : super(key: key);
+
+  @override
+  State<ThemeSwitch> createState() => _ThemeSwitchState();
+}
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  bool darkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      activeColor: Theme.of(context).primaryColor,
+      value: darkMode, 
+      onChanged: (status){
+        setState(() {
+          darkMode = status;
+        });
+      }
+    );
+  }
+}
+
+
