fa48eda6f5727ae1f943cec3b11f139171b5c79c
matteo2437 <matteo.budriesi@gmail.com>
2021-11-08 22:21:44 +0100

    create article preview card

--- lib/widgets/blog/article_preview.dart
+++ lib/widgets/blog/article_preview.dart
@@ -11,14 +11,56 @@ class ArticlePreview extends StatelessWidget {
   }) : super(key: key);
 
 
+  String convertDateTime(DateTime date){
+    String month = [
+      "Gen",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mag",
+      "Giu",
+      "Lug",
+      "Ago",
+      "Set",
+      "Ott",
+      "Nov",
+      "Dic",
+    ][date.month - 1];
+
+    String day = date.day.toString();
+    String year = date.year.toString(); 
+
+    return '$day $month $year';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           _getCover(),
-          _getPublishDate(),
-          Text(article.description),
+          Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _getTitle(context),
+                _getPublishDate(context),
+                _getTextPreview(context),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 8),
+            child: TextButton(
+              onPressed: () {},
+              child: const Text('APRI'),
+            ),
+          )
         ],
       ),
     );
@@ -27,16 +69,51 @@ class ArticlePreview extends StatelessWidget {
 
   Widget _getCover() {
     return article.hasImage
-      ? Image.network(
-          article.imageUrl!,
-          loadingBuilder: (context, widget, chunk) {
-            return const LinearProgressIndicator();
-          },
+      ? Container(
+          constraints: const BoxConstraints( 
+            maxHeight: 150,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(8),
+              topRight: Radius.circular(8),
+            ),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                article.imageUrl!,
+              )
+            )
+          ),
         )
       : const SizedBox.shrink();
   }
 
-  Widget _getPublishDate() {
-    return Text(article.publishDate.toString());
+  Widget _getPublishDate(context) {
+    return Text(
+      convertDateTime(article.publishDate),
+      style: Theme.of(context).textTheme.caption,
+    );
   }
+
+  Widget _getTitle(context) {
+    return Text(
+      article.title,
+      style: Theme.of(context).textTheme.headline6,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 2,
+    );
+  }  
+
+  Widget _getTextPreview(context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 8),
+      child: Text(
+        article.description,
+        style: Theme.of(context).textTheme.subtitle1,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 3,
+      ),
+    );
+  }  
 }
