abf2dd606b22c223c5f0ded29a44b65582c2ffd7
Stefano Calabretti <45517710+cala-br@users.noreply.github.com>
2021-11-09 17:16:03 +0100

    Change theme using theme bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bocconi_radio/blocs/app_theme.dart';
 import 'package:bocconi_radio/dependency_injection.dart';
 import 'package:bocconi_radio/widgets/app.dart';
 import 'package:flutter/material.dart';
@@ -9,29 +10,35 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final theme = ThemeData();
+  final _appTheme = getIt<AppTheme>();
 
   MyApp({Key? key}) : super(key: key);
  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bocconi Radio',
-      darkTheme: ThemeData(
-        colorScheme: const ColorScheme.dark(
-          primary: Colors.red,
-          secondary: Colors.red,
-        ),
-        primaryColor: Colors.red,
-        brightness: Brightness.dark,
-        backgroundColor: const Color(0xFF212121),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
-      ),
-      themeMode: ThemeMode.light,
-      home: const App(),
+    return StreamBuilder<ThemeMode>(
+      stream: _appTheme.values,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'Bocconi Radio',
+          darkTheme: ThemeData(
+            colorScheme: const ColorScheme.dark(
+              primary: Colors.red,
+              secondary: Colors.red,
+            ),
+            primaryColor: Colors.red,
+            brightness: Brightness.dark,
+            backgroundColor: const Color(0xFF212121),
+          ),
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+          ),
+          themeMode: snapshot.data!,
+          home: const App(),
+        );
+      }
     );
   }
 }
