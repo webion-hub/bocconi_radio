53fc3c185138a5d40d4b716bb9a6f45f0eec41d3
matteo2437 <matteo.budriesi@gmail.com>
2021-11-10 17:59:53 +0100

    create image with loading

---
+++ lib/widgets/image_with_loading.dart
@@ -0,0 +1,56 @@
+import 'package:bocconi_radio/widgets/util.dart';
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ImageWithLoading extends StatelessWidget {
+  final double height;
+  final String src;
+  final BorderRadius borderRadius;
+
+  const ImageWithLoading({ 
+    required this.height,
+    required this.src,
+    this.borderRadius = BorderRadius.zero,
+    Key? key 
+  }) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final image = NetworkImage(src);
+    final _streamImageLoader = BehaviorSubject<bool>();
+
+    image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener(
+        (info, call) {
+          _streamImageLoader.add(true);
+        },
+      ),
+    );
+
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: image
+        )
+      ),
+      child: StreamBuilder(
+        stream: _streamImageLoader,
+        initialData: false,
+        builder: (context, AsyncSnapshot snapshot){
+          return MaybeShow(
+            show: !snapshot.data, 
+            child: const Center(
+              child: CircularProgressIndicator()
+            ),
+            alternativeChild: Container(),
+          );
+        },
+      ),
+    );
+  }
+}
