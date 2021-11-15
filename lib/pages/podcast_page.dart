3ae80f1db650425ca5f06d2386438114cb233b80
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-11-15 16:41:27 +0100

    Refactor

--- lib/pages/podcast_page.dart
+++ lib/pages/podcast_page.dart
@@ -1,69 +1,81 @@
 import 'package:audio_service/audio_service.dart';
+import 'package:bocconi_radio/dependency_injection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 
-late AudioHandler audioHandler;
-
 class PodcastPage extends StatelessWidget {
-  const PodcastPage({Key? key})
+  final _audioHandler = getIt<AudioHandler>();
+  
+  PodcastPage({Key? key})
     : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Audio Service Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Show media item title
-            StreamBuilder<MediaItem?>(
-              stream: audioHandler.mediaItem,
-              builder: (context, snapshot) {
-                final mediaItem = snapshot.data;
-                return Text(mediaItem?.title ?? '');
-              },
-            ),
-            // Play/pause/stop buttons.
-            StreamBuilder<bool>(
-              stream: audioHandler.playbackState
-                  .map((state) => state.playing)
-                  .distinct(),
-              builder: (context, snapshot) {
-                final playing = snapshot.data ?? false;
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _button(Icons.fast_rewind, audioHandler.rewind),
-                    if (playing)
-                      _button(Icons.pause, audioHandler.pause)
-                    else
-                      _button(Icons.play_arrow, audioHandler.play),
-                    _button(Icons.stop, audioHandler.stop),
-                    _button(Icons.fast_forward, audioHandler.fastForward),
-                  ],
-                );
-              },
-            ),
-            // Display the processing state.
-            StreamBuilder<AudioProcessingState>(
-              stream: audioHandler.playbackState
-                  .map((state) => state.processingState)
-                  .distinct(),
-              builder: (context, snapshot) {
-                final processingState =
-                    snapshot.data ?? AudioProcessingState.idle;
-                return Text(
-                    "Processing state: ${describeEnum(processingState)}");
-              },
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Show media item title
+          StreamBuilder<MediaItem?>(
+            stream: _audioHandler.mediaItem,
+            builder: (context, snapshot) {
+              final mediaItem = snapshot.data;
+              return Text(mediaItem?.title ?? '');
+            },
+          ),
+          // Play/pause/stop buttons.
+          StreamBuilder<bool>(
+            stream: _audioHandler
+              .playbackState
+              .map((state) => state.playing)
+              .distinct(),
+
+            builder: (context, snapshot) {
+              final playing =
+                snapshot.data ?? false;
+              
+              return _controlButtons(playing); 
+            },
+          ),
+          // Display the processing state.
+          StreamBuilder<AudioProcessingState>(
+            stream: _audioHandler
+              .playbackState
+              .map((state) => state.processingState)
+              .distinct(),
+            
+            builder: (context, snapshot) {
+              final processingState =
+                snapshot.data ?? AudioProcessingState.idle;
+              
+              return Text(
+                'Processing state: ${describeEnum(processingState)}'
+              );
+            },
+          ),
+        ],
       ),
     );
+  }
+
+
+  Widget _controlButtons(bool playing) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _button(Icons.fast_rewind, _audioHandler.rewind),
+        _playOrPauseButton(playing),
+        _button(Icons.stop, _audioHandler.stop),
+        _button(Icons.fast_forward, _audioHandler.fastForward),
+      ],
+    );
+  }
+
+  Widget _playOrPauseButton(bool playing) {
+    return playing
+      ? _button(Icons.pause, _audioHandler.pause)
+      : _button(Icons.play_arrow, _audioHandler.play);
   }
 
   IconButton _button(IconData iconData, VoidCallback onPressed) {
