8866a743017c167f946d31ba8436caaeed299791
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-11-17 17:14:51 +0100

    Remove unused dependency

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -1,7 +1,6 @@
 import 'package:bocconi_radio/widgets/info/bocconi_radio_icons.dart';
 import 'package:bocconi_radio/widgets/info/info_title.dart';
 import 'package:bocconi_radio/widgets/theme_switch.dart';
-import 'package:darq/src/extensions/concat.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
